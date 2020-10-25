39ce0de7bfb9579982f73bb8cb0d2276630b4ee2
Guillaume  Schpektor <guillaumeschpektor@Guillaumes-MacBook-Pro.local>
2020-10-25 21:09:17 +0200

    week 2

---
+++ week.2/day.1/daily.challenge/script.js
@@ -0,0 +1,34 @@
+//Question 1
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// let Banana = fruits.shift()
+// console.log(fruits)
+
+//Question 2
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// let abc = fruits.sort()
+// console.log(fruits)
+
+// Question 3
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// let kiwi = fruits.push('kiwi')
+// console.log(fruits)
+
+// Question 4
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// let Apples = fruits.splice(1, 1)
+// console.log(fruits)
+
+//Question 5
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// let backwards = fruits.reverse()
+// console.log(fruits)
+
+//Question 6
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits [1] [1] [0])
---
+++ week.2/day.1/script.js
@@ -0,0 +1,124 @@
+//Exercise 1
+
+
+ 	// 5 + "34" ===== 534
+    // console.log(5 - "4") ==== 1
+    // console.log(10 % 5) ===== 0
+    // console.log(5 % 10) ===== 5
+    // console.log("Java" + "Script") ==== javascript
+    // console.lo    // console.log("Bob" - "bill") ==== NaNg(" " + " ") ====   
+    // console.log(" " + 0)
+    // console.log(true + true) ==== 2
+    // console.log(true + false) === 1
+    // false + true === 1
+    // console.log(false - true) === -1
+    // console.log(3 - 4) ==== -1
+
+
+//Exercise 2
+
+// let me = ["my","favorite","color","is","blue"]
+
+// let sentence = me.join(" ")
+// console.log(sentence)
+
+//Exercise 3
+
+// let str1 = "mix";
+// let str2 = "pod";
+
+// let temp = str2.slice(0, 2) + str1.slice(2, 3) + " " + str1.slice(0, 2) + str2.slice(2, 3);
+
+
+// console.log(temp)
+
+
+
+//Ecercise 4
+
+// Prompt the user for the first number.
+// Store that first number
+// Prompt the user for the second number.
+// Store that second number
+// Alert the result of the SUM of these two numbers.
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+// let nr1 = parseInt(prompt('number'))
+// let nr2 = parseInt(prompt('number'))
+
+// let sum = alert(nr1 * nr2)
+
+// console.log(sum)
+
+
+//Exercise 5
+
+// You’re given a string of words. You need to find the word “Nemo”, and return a string like this: “I found Nemo at 
+// [the order of the word you find nemo]!”.
+
+// Bonus : If you can’t find Nemo, console.log “I can’t find Nemo :(“.
+
+// Hint : use an if/else statement
+
+// let sentence = "I am finding Nemo!"
+// let why = sentence.split(" ")
+// let find = why.indexOf("Nemo")
+
+// console.log('I found nemo at' + " " + find)
+
+   // var string = "I am Nemo not a fish.";
+   //  var split = string.split(" ");
+   //  var n = split.indexOf("Nemo");
+   //      if(n>0){
+   //          console.log("I found Nemo at" + " " + n + "!");
+   //      }
+   //      else{
+   //          console.log("Nemo is not here")}
+
+
+// Exercise 6
+
+    // console.log(5 >= 1) === true
+    // console.log(0 === 1) === false
+    // console.log(4 <= 1) === false
+    // console.log(1 != 1) === false
+    // console.log("A" > "B") === false
+    // console.log("B" < "C") === true
+    // console.log("a" > "A") === true
+    // console.log("b" < "A") === false
+    // console.log(true === false) === false
+
+
+// Exercise 7
+
+ 	// let c;
+  //   let a = 34;
+  //   let b = 21;
+  //   a = 2;
+    
+    // console.log(3 + 4 + '5');
+
+// Exercise 8
+
+
+// let ask = prompt('give me a string of numbers seperated by a comma and space')
+// let sep = ask.split(", ")
+// console.log(sep[0]*sep[1])
+
+
+//Exercise 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
